4d6b80b922c77ceed221ffbf2815d64bba2101ab
Ignacio Alonso <ignacio.alley@gmail.com>
2020-07-11 17:02:00 -0500

    Final config

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,7 +1,7 @@
 class Cocktail < ApplicationRecord
   validates :name, uniqueness: true, presence: true
 
-  has_many :doses, dependent: :destroy
+  has_many :doses, dependent: :destroy # cuando borremos cocktail, se van a borrar todas las doses que tiene
   has_many :ingredients, through: :doses
   has_many :reviews, dependent: :destroy
 end
